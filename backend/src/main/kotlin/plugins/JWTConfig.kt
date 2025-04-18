7b98863cf220a8d81fdf22d26361ad7c0c6e5b12
A-man404 <amansaini04a@gmail.com>
2025-04-18 18:13:23 +0530

    Created JWT Authentication

---
+++ backend/src/main/kotlin/plugins/JWTConfig.kt
@@ -0,0 +1,23 @@
+package plugins
+
+import io.ktor.server.application.*
+
+fun Application.JWTConfig(): JWTConfig {
+
+
+    val jwt = environment.config.config("ktor.jwt")
+    val realm = jwt.property("realm").getString()
+    val issuer = jwt.property("issuer").getString()
+    val secret = jwt.property("secret").getString()
+    val audience = jwt.property("audience").getString()
+    val tokenExpiry = jwt.property("expiry").getString()
+
+
+    return JWTConfig(
+        realm = realm,
+        secret = secret,
+        issuer = issuer,
+        audience = audience,
+        tokenExpiry = tokenExpiry.toLong()
+    )
+}
