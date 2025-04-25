04968f9a1a97bb268e1bbf909a1602253e5a1ab5
A-man404 <amansaini04a@gmail.com>
2025-04-25 17:49:28 +0530

    Created Color Palette

---
+++ app/composeApp/src/commonMain/kotlin/com/example/ui/theme/ColorPalette.kt
@@ -0,0 +1,13 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val backgroundColor = Color(0xFF1E1E2E)
+val primaryPastel = Color(0xFFFBE7A1)
+val accentPastel = Color(0xFFBCAEFF)
+val cardColor = Color(0xFF2B2B3C)
+val accentColor = Color(0xFFBCAEFF)
+val successColor = Color(0xFF9AEBA3)
+val errorColor = Color(0xFFFF8A8A)
+
+
